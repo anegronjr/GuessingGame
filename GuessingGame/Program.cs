f224cd3c4dc63badf774f1118ce723c6167ce38d
Artie Negron <anegroncsu@gmail.com>
2019-01-16 14:30:50 -0500

    Minor changes.

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -6,8 +6,19 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Adios!");
+            //First iteration: If user guesses 7, they win. If not, they lose.
+
+            Console.WriteLine("Please guess a number between 1 and 10.");
+            string userGuess = Console.ReadLine();
+            Console.WriteLine("You guessed: " + userGuess);
+
+            if (userGuess.Equals("7"))
+            {
+                Console.WriteLine("You win!");
+            } else
+            {
+                Console.WriteLine("You lose.");
+            }
         }
     }
 }
